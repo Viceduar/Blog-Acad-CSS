623d2eaa7277290d9a64dabba13744f885788981
José <josedlesp@gmail.com>
2023-06-17 14:19:29 -0600

    Bontón de return en vue

--- js/app.js
+++ js/app.js
@@ -34,149 +34,21 @@ const { createApp } = Vue
             if (!this.isMobile) {
                 body.classList.toggle("non-scroll")
             }
+        },
+
+        /**
+         * When called, takes the user to the beginning of the page
+         */
+        // This function is called by the return button
+        scrollBack(){
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
         }
 
     }
   }).mount('#app')
 
 
-/***********CONFIGURACIÓN DE BTN-RETURN*******************/
 
-// Al hacer click en el botón de regreso, esta función nos llevará al inicio de la página.
-const btnReturn = document.querySelector(".btn-return");
-
-btnReturn.addEventListener("click", function returnButton () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
-
-/***********CLICK EN BOTON DE LIKE*******************/
-
-
-// Cuando demos click en un elemento que sea un botón, a este se le agregará una nueva clase 
-// que nos indicará que ya se ha dado like.
-document.addEventListener('click', function buttonLiked(event) {
-  if(event.target.classList.contains('btn-like')){
-    event.target.classList.toggle('btn-liked')
-  }else if(event.target.parentElement.classList.contains('btn-like')){
-    event.target.parentElement.classList.toggle('btn-liked')
-  }
-});
-
-
-
-
-// *****************************************
-// ****************CAROUSEL*****************
-// *****************************************
-const itemsEl = document.querySelectorAll(".carousel-item");
-const btnLeft = document.querySelector(".btn-carousel--left");
-const btnRight = document.querySelector(".btn-carousel--right");
-
-const mediaQueryPhone = 600; //600px phone
-const mediaQueryTabPort = 900; //900px tab-port
-const mediaQueryTabLand = 1200; //1200px tab-land
-const mediaQueryDesktop = 1800; // 1800px desktop
-
-let elemPerView;
-
-//Scroll counters
-let scrollsLeft;
-let scrollsRight;
-let maxScrolls;
-
-//Detect currrent MEDIAQUERY
-const mediaSensor = function (width) {
-  if (width <= mediaQueryPhone) {
-    return mediaQueryPhone;
-  } else if (width <= mediaQueryTabPort) {
-    return mediaQueryTabPort;
-  } else if (mediaQueryTabPort < width) {
-    return mediaQueryDesktop;
-  }
-};
-
-//Get the current MEDIAQUERY
-let currentQuery = mediaSensor(window.innerWidth);
-
-//Build the carousel according with MEDIAQUERY
-const carouselInit = function () {
-
-  //For phone
-  if (currentQuery <= mediaQueryPhone) {
-    //only one card in carousel per view
-    elemPerView = 1;
-  } 
-  
-  //For Tab-port
-  else if (currentQuery <= mediaQueryTabPort) {
-    //only two cards in carousel per view
-    elemPerView = 2;
-  } 
-  
-  //For Desktop
-  else if (currentQuery <= mediaQueryDesktop) {
-    //Three cards per view
-    elemPerView = 3;
-  }
-
-  //Determine how many scrolls can allow according with cards per view
-  maxScrolls = Math.ceil(itemsEl.length / elemPerView) - 1;
-
-  scrollsLeft = maxScrolls;
-  scrollsRight = 0;
-
-
-  //Change size of cards
-  itemsEl.forEach((c, i) => {
-    c.style.left = `${(100 / elemPerView) * i}%`;
-    c.style.width = `${100 / elemPerView}%`;
-  });
-};
-
-carouselInit();
-
-//Detect change in width of the window
-window.addEventListener("resize", function () {
-  if (currentQuery != mediaSensor(window.innerWidth)) {
-    currentQuery = mediaSensor(window.innerWidth);
-    carouselInit();
-  }
-});
-
-//BUTTONS CONTROLS
-
-//Move to right
-btnRight.addEventListener("click", function () {
-
-  //If you're not in max of scrolls
-  if (scrollsRight != maxScrolls) {
-    //Translate all cards to left
-    itemsEl.forEach((item) => {
-      item.style.left = `calc(${item.style.left} + ${-100}%)`;
-    });
-
-
-    //Update scrolls counters
-    scrollsLeft--;
-    scrollsRight++;
-  }
-});
-
-//Move to left
-btnLeft.addEventListener("click", function () {
-
-  //if you're not in max of scrolls
-  if (scrollsLeft != maxScrolls) {
-    //Translate all cards to right
-    itemsEl.forEach((item) => {
-      item.style.left = `calc(${item.style.left} + ${100}%)`;
-    });
-
-    //Update scrolls counters
-    scrollsLeft++;
-    scrollsRight--;
-  }
-});
---
+++ js/carousel.js
@@ -0,0 +1,112 @@
+// *****************************************
+// ****************CAROUSEL*****************
+// *****************************************
+const itemsEl = document.querySelectorAll(".carousel-item");
+const btnLeft = document.querySelector(".btn-carousel--left");
+const btnRight = document.querySelector(".btn-carousel--right");
+
+const mediaQueryPhone = 600; //600px phone
+const mediaQueryTabPort = 900; //900px tab-port
+const mediaQueryTabLand = 1200; //1200px tab-land
+const mediaQueryDesktop = 1800; // 1800px desktop
+
+let elemPerView;
+
+//Scroll counters
+let scrollsLeft;
+let scrollsRight;
+let maxScrolls;
+
+//Detect currrent MEDIAQUERY
+const mediaSensor = function (width) {
+  if (width <= mediaQueryPhone) {
+    return mediaQueryPhone;
+  } else if (width <= mediaQueryTabPort) {
+    return mediaQueryTabPort;
+  } else if (mediaQueryTabPort < width) {
+    return mediaQueryDesktop;
+  }
+};
+
+//Get the current MEDIAQUERY
+let currentQuery = mediaSensor(window.innerWidth);
+
+//Build the carousel according with MEDIAQUERY
+const carouselInit = function () {
+
+  //For phone
+  if (currentQuery <= mediaQueryPhone) {
+    //only one card in carousel per view
+    elemPerView = 1;
+  } 
+  
+  //For Tab-port
+  else if (currentQuery <= mediaQueryTabPort) {
+    //only two cards in carousel per view
+    elemPerView = 2;
+  } 
+  
+  //For Desktop
+  else if (currentQuery <= mediaQueryDesktop) {
+    //Three cards per view
+    elemPerView = 3;
+  }
+
+  //Determine how many scrolls can allow according with cards per view
+  maxScrolls = Math.ceil(itemsEl.length / elemPerView) - 1;
+
+  scrollsLeft = maxScrolls;
+  scrollsRight = 0;
+
+
+  //Change size of cards
+  itemsEl.forEach((c, i) => {
+    c.style.left = `${(100 / elemPerView) * i}%`;
+    c.style.width = `${100 / elemPerView}%`;
+  });
+};
+
+carouselInit();
+
+//Detect change in width of the window
+window.addEventListener("resize", function () {
+  if (currentQuery != mediaSensor(window.innerWidth)) {
+    currentQuery = mediaSensor(window.innerWidth);
+    carouselInit();
+  }
+});
+
+//BUTTONS CONTROLS
+
+//Move to right
+btnRight.addEventListener("click", function () {
+
+  //If you're not in max of scrolls
+  if (scrollsRight != maxScrolls) {
+    //Translate all cards to left
+    itemsEl.forEach((item) => {
+      item.style.left = `calc(${item.style.left} + ${-100}%)`;
+    });
+
+
+    //Update scrolls counters
+    scrollsLeft--;
+    scrollsRight++;
+  }
+});
+
+//Move to left
+btnLeft.addEventListener("click", function () {
+
+  //if you're not in max of scrolls
+  if (scrollsLeft != maxScrolls) {
+    //Translate all cards to right
+    itemsEl.forEach((item) => {
+      item.style.left = `calc(${item.style.left} + ${100}%)`;
+    });
+
+    //Update scrolls counters
+    scrollsLeft++;
+    scrollsRight--;
+  }
+});
